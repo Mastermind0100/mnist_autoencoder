62f7fdb5d144c730cee053354d0602754563ec41
Atharva <atharvahudlikar@gmail.com>
2020-02-26 13:34:13 +0530

    Test code modified

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import cv2
 
 model = load_model('wts.h5')
 img = cv2.imread('test.png')
-cv2.resize(img,(28,28))
-cv2.imshow('w',img)
+resized = cv2.resize(img,(28,28))
+cv2.imshow('w',resized)
 cv2.waitKey()
 cv2.destroyAllWindows()
